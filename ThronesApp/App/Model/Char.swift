f38f087f00b786edd9d78a88a06adf6deb4b0f85
Diggo Silva <rdiggosilva@gmail.com>
2025-10-16 09:15:21 -0300

    Created CharResponse object

---
+++ ThronesApp/App/Model/Char.swift
@@ -0,0 +1,6 @@
+struct Char {
+    let id: Int
+    let firstName, lastName, fullName, title: String
+    let family, image: String
+    let imageUrl: String
+}
---
+++ ThronesApp/App/Network/Service.swift
@@ -0,0 +1,8 @@
+//
+//  Service.swift
+//  ThronesApp
+//
+//  Created by Diggo Silva on 15/10/25.
+//
+
+import Foundation

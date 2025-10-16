0d355fdd11bc4f6ca064587b366c3b87ae6112e0
Diggo Silva <rdiggosilva@gmail.com>
2025-10-16 09:15:56 -0300

    Created Service file

--- ThronesApp/App/Network/Service.swift
+++ ThronesApp/App/Network/Service.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+final class Service {
+    
+    func getCharacters() async throws -> [Char] {
+        guard let url = URL(string: "https://thronesapi.com/api/v2/Characters") else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw URLError(.badServerResponse)
+        }
+        
+        let charResponse = try JSONDecoder().decode([CharResponse].self, from: data)
+        var charList: [Char] = []
+        
+        for char in charResponse {
+            let char = Char(
+                id: char.id,
+                firstName: char.firstName,
+                lastName: char.lastName,
+                fullName: char.fullName,
+                title: char.title,
+                family: char.family,
+                image: char.image,
+                imageUrl: char.imageURL
+            )
+            charList.append(char)
+        }
+        return charList
+    }
+}

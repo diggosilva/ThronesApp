dd2da85e904a14f48232e1fc30a1c083ed37a78d
Diggo Silva <rdiggosilva@gmail.com>
2025-10-17 08:39:13 -0300

    Created StatefulViewModel file

--- ThronesApp/App/Utils/StatefulViewModel.swift
+++ ThronesApp/App/Utils/StatefulViewModel.swift
@@ -5,4 +5,9 @@
 //  Created by Diggo Silva on 16/10/25.
 //
 
-import Foundation
+import Combine
+
+protocol StatefulViewModel {
+    associatedtype State
+    var statePublisher: AnyPublisher<State, Never> { get }
+}

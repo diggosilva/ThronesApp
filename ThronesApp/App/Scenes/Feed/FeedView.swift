e93f2f8b1d82c66912f4e50772c8a0157355f2fb
Diggo Silva <rdiggosilva@gmail.com>
2025-10-18 12:31:57 -0300

    Connected FeedView with FeedViewController

---
+++ ThronesApp/App/Scenes/Feed/FeedView.swift
@@ -0,0 +1,8 @@
+//
+//  FeedView.swift
+//  ThronesApp
+//
+//  Created by Diggo Silva on 17/10/25.
+//
+
+import Foundation
--- ThronesApp/App/Scenes/Feed/FeedViewController.swift
+++ ThronesApp/App/Scenes/Feed/FeedViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    private let feedView = FeedView()
+    
+    override func loadView() {
+        view = feedView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setNavBar()
+        setDataSourcesAndDelegates()
     }
+    
+    private func setNavBar() {
+        navigationItem.title = "Game Of Thrones"
+    }
+    
+    private func setDataSourcesAndDelegates() {
+        feedView.collectionView.dataSource = self
+        feedView.collectionView.delegate = self
+    }
+}
 
+extension FeedViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemIndigo
+        return cell
+    }
 }

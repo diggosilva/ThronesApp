0b250c51657265db07d9dab3b579a4ce60f20542
Diggo Silva <rdiggosilva@gmail.com>
2025-10-27 07:37:59 -0300

    Created DetailsViewController and connected with View and ViewModel

--- ThronesApp/App/Scenes/Details/DetailsViewController.swift
+++ ThronesApp/App/Scenes/Details/DetailsViewController.swift
@@ -5,4 +5,30 @@
 //  Created by Diggo Silva on 26/10/25.
 //
 
-import Foundation
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    private let detailsView = DetailsView()
+    private let viewModel: DetailsViewModelProtocol
+    
+    init(viewModel: DetailsViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    override func loadView() {
+        view = detailsView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCharacter()
+    }
+    
+    private func setupCharacter() {
+        detailsView.configure(char: viewModel.getCharacter())
+    }
+}

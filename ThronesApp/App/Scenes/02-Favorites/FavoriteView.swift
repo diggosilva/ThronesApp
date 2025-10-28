ddd78b9a816cb07db8cb385d819464225a85cd9b
Diggo Silva <rdiggosilva@gmail.com>
2025-10-28 08:51:37 -0300

    Created FavoriteView file

---
+++ ThronesApp/App/Scenes/02-Favorites/FavoriteView.swift
@@ -0,0 +1,44 @@
+//
+//  FavoriteView.swift
+//  ThronesApp
+//
+//  Created by Diggo Silva on 27/10/25.
+//
+
+import UIKit
+
+class FavoriteView: UIView {
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .insetGrouped)
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        addSubview(tableView)
+        backgroundColor = .systemBackground
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}

69e37384b2b3bc7ce709cbe6afe2c16a53cbab8d
Diggo Silva <rdiggosilva@gmail.com>
2025-10-30 09:22:06 -0300

    Created TabBar file

--- ThronesApp/App/Scenes/0-TabBar/TabBarController.swift
+++ ThronesApp/App/Scenes/0-TabBar/TabBarController.swift
@@ -5,4 +5,27 @@
 //  Created by Diggo Silva on 29/10/25.
 //
 
-import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    let vc1 = UINavigationController(rootViewController: FeedViewController())
+    let vc2 = UINavigationController(rootViewController:FavoriteViewController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configViewControllers()
+    }
+    
+    private func configViewControllers() {
+        vc1.tabBarItem.title = "Personagens"
+        vc1.tabBarItem.image = UIImage(systemName: "person.2")
+        vc1.tabBarItem.selectedImage = UIImage(systemName: "person.2.fill")
+        
+        vc2.tabBarItem.title = "Favoritos"
+        vc2.tabBarItem.image = UIImage(systemName: "star")
+        vc2.tabBarItem.selectedImage = UIImage(systemName: "star.fill")
+        
+        viewControllers = [vc1, vc2]
+    }
+}

ea8bfa520fa6357673736194fb1673f33bd05d4a
Diggo Silva <rdiggosilva@gmail.com>
2025-11-26 09:50:07 -0300

    Refactored FavoriteViewController

--- ThronesApp/App/Scenes/02-Favorites/FavoriteViewController.swift
+++ ThronesApp/App/Scenes/02-Favorites/FavoriteViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 class FavoriteViewController: UIViewController {
     
     private let favoriteView = FavoriteView()
+    private let viewModel: FavoritesViewModelProtocol
+    
+    init(viewModel: FavoritesViewModelProtocol = FavoriteViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
     override func loadView() {
         view = favoriteView
@@ -21,6 +29,12 @@ class FavoriteViewController: UIViewController {
         configureDelegatesAndDataSources()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.setDelegate(self)
+        viewModel.loadChars()
+    }
+    
     private func configureNavigationBar() {
         navigationItem.title = "Favoritos"
     }
@@ -28,23 +42,31 @@ class FavoriteViewController: UIViewController {
     private func configureDelegatesAndDataSources() {
         favoriteView.tableView.dataSource = self
         favoriteView.tableView.delegate = self
+        favoriteView.tableView.reloadData()
     }
 }
 
-extension FavoriteViewController: UITableViewDataSource, UITableViewDelegate {
+extension FavoriteViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 16
+        return viewModel.numberOfRows()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        var content = cell.defaultContentConfiguration()
-        content.text = "Linha \(indexPath.row + 1)"
-        content.secondaryText = "Casa"
-        content.image = UIImage(systemName: "star.fill")
-        
-        cell.contentConfiguration = content
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteCell.identifier, for: indexPath) as? FavoriteCell else { return UITableViewCell() }
+        let char = viewModel.cellForRow(at: indexPath.row)
+        cell.configure(char: char)
         return cell
+    }
+}
+
+extension FavoriteViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension FavoriteViewController: FavoriteViewModelDelegate {
+    func reloadTable() {
+        favoriteView.tableView.reloadData()
     }
 }

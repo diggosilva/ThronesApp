cbf2ad8b30518f89940207e561cff800b914549b
Diggo Silva <rdiggosilva@gmail.com>
2025-11-30 10:53:12 -0300

    Xcode configutarion

---
+++ ThronesAppTests/FeedViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  ThronesAppTests.swift
+//  ThronesAppTests
+//
+//  Created by Diggo Silva on 30/11/25.
+//
+
+import Testing
+
+struct ThronesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

ba5666c1149b9ca834c20f479fe06e472144c09c
Diggo Silva <rdiggosilva@gmail.com>
2025-11-30 11:04:42 -0300

    Separated MockService to another file

--- ThronesAppTests/MockService.swift
+++ ThronesAppTests/MockService.swift
@@ -5,4 +5,22 @@
 //  Created by Diggo Silva on 30/11/25.
 //
 
-import Foundation
+import XCTest
+@testable import ThronesApp
+
+class MockService: ServiceProtocol {
+    
+    var isSuccess: Bool = true
+    var isFiltering: Bool = false
+    
+    func getCharacters() async throws -> [Char] {
+        if isSuccess {
+            return [
+                Char(id: 1, firstName: "Mario", lastName: "Bros", fullName: "Mario Bros", title: "", family: "", image: "", imageUrl: ""),
+                Char(id: 1, firstName: "Luigi", lastName: "Bros", fullName: "Luigi Bros", title: "", family: "", image: "", imageUrl: ""),
+            ]
+        } else {
+            throw DSError.unknown
+        }
+    }
+}

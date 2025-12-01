b26fd948bce1c2379d6efdb0074436dec55e6d82
Diggo Silva <rdiggosilva@gmail.com>
2025-12-01 13:49:52 -0300

    Created Mock Repository file

--- ThronesAppTests/MockRepository.swift
+++ ThronesAppTests/MockRepository.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+@testable import ThronesApp
+
+final class RepositoryMock: RepositoryProtocol {
+
+    var savedChars: [Char] = []
+    var shouldThrowOnSave = false
+    var saveCharCalled = false
+
+    func getChars() -> [Char] {
+        return savedChars
+    }
+
+    func saveChar(_ char: Char) async throws {
+        saveCharCalled = true
+        
+        if shouldThrowOnSave {
+            throw DSError.charAlreadyExists
+        }
+        
+        savedChars.append(char)
+    }
+
+    func saveChars(_ chars: [Char]) {
+        savedChars = chars
+    }
+}

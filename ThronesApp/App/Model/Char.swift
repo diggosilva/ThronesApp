d4fe2a80b6ec2489763d7250d360cf62c9d112b0
Diggo Silva <rdiggosilva@gmail.com>
2025-12-01 13:49:26 -0300

    Implemented Equatable for object

--- ThronesApp/App/Model/Char.swift
+++ ThronesApp/App/Model/Char.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Char: Codable {
+struct Char: Codable, Equatable {
     let id: Int
     let firstName, lastName, fullName, title: String
     let family, image: String
---
+++ ThronesAppTests/MockRepository.swift
@@ -0,0 +1,8 @@
+//
+//  MockRepository.swift
+//  ThronesAppTests
+//
+//  Created by Diggo Silva on 01/12/25.
+//
+
+import Foundation
